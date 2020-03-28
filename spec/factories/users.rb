cb1e9c48f22900359f84fac5511221b1c24a256b
EC2 Default User <ec2-user@ip-172-31-41-248.ap-northeast-1.compute.internal>
2020-03-28 07:49:16 +0000

    fix

--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    sequence(:email) { |n| "TEST#{n}@example.com"}
-    sequence(:password) { "password" }
-    sequence(:password_confirmation) { "password" }
-  end
-end
